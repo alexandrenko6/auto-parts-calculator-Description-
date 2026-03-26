2091cf40093bb2ab2f028a090f5c1a9dcea4f7f1
alexandrenko6 <sanya.kostrov0@gmail.com>
2026-03-26 10:27:50 +0200

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,22 @@
+Модуль констант для калькулятора автозапчастей.
+"""
 
+# Тарифы на доставку
+DELIVERY_RATES = {
+    1: {"name": "Обычная", "rate_per_kg": 10.0, "rate_per_km": 5.0, "multiplier": 1.0},
+    2: {"name": "Экспресс", "rate_per_kg": 10.0, "rate_per_km": 5.0, "multiplier": 1.5},
+    3: {"name": "Самовывоз", "rate_per_kg": 0.0, "rate_per_km": 0.0, "multiplier": 0.0}
+}
+
+# Промокоды
+PROMOCODES = {
+    "SALE10": 10,
+    "SALE20": 20,
+    "WELCOME": 5,
+    "BLACKFRIDAY": 25
+}
+
+# Дополнительные услуги
+INSURANCE_PERCENT = 0.05
+URGENT_FEE = 500.0
+FREE_DELIVERY_THRESHOLD = 5000.0
